72f9ab309fcfa2d2b804a9a55827b74424d304fb
Brad Trewhella <brad.trewhella@gmail.com>
2017-08-05 15:40:08 +1000

    fit initial model in modelz.R

--- modelz.R
+++ modelz.R
@@ -1,10 +1,13 @@
+library(cluster)
 library(xgboost)
-
+library(lubridate)
+library(tidyverse)
+library(FeatureHashing)
 transactions <- read_csv("~/Documents/Kaggle/zillow/data/train_2016_v2.csv")
 
-properties <- read_csv("~/Documents/Kaggle/zillow/data/properties_2016.csv")
+temp_dist <- read_csv("~/Documents/Kaggle/zillow/data/properties_2016.csv")
 
-properties <- properties %>% rename(
+temp_dist <- temp_dist %>% rename(
   id_parcel = parcelid,
   build_year = yearbuilt,
   area_basement = basementsqft,
@@ -56,107 +59,46 @@ properties <- properties %>% rename(
   aircon = airconditioningtypeid,
   architectural_style= architecturalstyletypeid
 )
-transactions <- transactions %>% rename(
+
+transactions <- transactions %>% dplyr::rename(
   id_parcel = parcelid,
   date = transactiondate
 )
 
-properties <- properties %>% 
+temp_dist <- temp_dist %>% 
   mutate(tax_delinquency = ifelse(tax_delinquency=="Y",1,0),
          flag_fireplace = ifelse(flag_fireplace=="Y",1,0),
          flag_tub = ifelse(flag_tub=="Y",1,0))
 
-transactions %>% mutate(month = as.factor(month(date))) %>% lm(logerror~month, data = .) %>% 
-  summary
 
 
-properties %>% 
-  inner_join(transactions) %>% 
-  nrow
-  
-properties %>%
-  inner_join(transactions) %>% 
-  group_by(zoning_property) %>% 
-  summarise(n=n(),
-            var = var(logerror),
-            mean = mean(logerror)) %>%
-  arrange(desc(n), desc(var)) %>% View
-  
-  
-properties %>% inner_join(transactions) %>% group_by(region_zip) %>% 
-  summarise(n()) %>% View
-
-
-library(leaflet)
-
-properties %>% filter(region_zip==97319) %>% inner_join(transactions) %>% 
-  summarise(max(logerror), min(logerror), mean(logerror))
-  arrange(longitude) %>%
-  ggplot(aes(y= latitude, x=longitude, color=logerror)) + geom_point() +
-  scale_color_continuous(limits = c())
-
-  
-properties %>% filter(region_zip==97328) %>% inner_join(transactions) %>% 
-    ggplot(aes(y= latitude, x=longitude, color=logerror)) + geom_point() +
-    scale_color_continuous(limits = c())
-  
-  
-    96987
-properties %>% filter(region_zip==97319) %>%  inner_join(transactions) %>% .[668,] %>%  pull(logerror)
-
-
-
-properties %>% filter(region_zip==97328) %>% inner_join(transactions) %>%  .[83,] %>% 
-  pull(logerror)
-  pull(latitude) %>% which.min
-  %>%  pull(latitude) %>% which.min
-  
-  
-properties %>% filter(region_zip==97328) %>% 
-  inner_join(transactions) %>% summarise(sd(logerror))
-
-  
-  
-  
-  
-# distance from centre of zip/n'hood/ - flag if more than 2 stds of average - mahalanobis
-
-
-library(cluster)
-?mahalanobis
-
-prop97328 <- properties %>% filter(region_zip==97328)  %>% inner_join(transactions)
-
-S <- matrix(c(var(prop97328$latitude),
-              cov(prop97328$latitude,prop97328$longitude),
-              cov(prop97328$latitude,prop97328$longitude),
-              var(prop97328$longitude)
-              ), nrow = 2, byrow = TRUE)
-mu <- c(mean(prop97328$latitude), mean(prop97328$longitude))
 
 
 detect_outliers <- function(dat) {
-  if(nrow(dat) <10) {
+  if(nrow(dat) <50) {
+    dat$is_outlier <- 0
+    dat$mahal_dist <- 0
     return(dat)
   }
-  if((var(dat$latitude) + var(dat$longitude)) < 10){
+  if((var(dat$latitude, na.rm = TRUE) + var(dat$longitude, na.rm = TRUE)) < 50) {
+    dat$is_outlier <- 0
+    dat$mahal_dist <- 0
     return(dat)
   }
-  S <- matrix(c(var(dat$latitude),
-                cov(dat$latitude,  dat$longitude),
-                cov(dat$latitude, dat$longitude),
-                var(dat$longitude)
-  ), nrow = 2, byrow = TRUE)
-  mu <- c(mean(dat$latitude), mean(dat$longitude))
-  dists <- mahalanobis(cbind(dat$latitude, dat$longitude), center = mu, cov = S)  
+  lat_and_lon <- cbind(dat$latitude,dat$longitude)
+  S <- cov(lat_and_lon)
+  mu <- colMeans(lat_and_lon)
+  dists <- mahalanobis(lat_and_lon, center = mu, cov = S)  
   sd_mh <- sd(dists)
   avg_mh <- mean(dists)
-  lb <- avg_mh - 3*sd_mh
+  ub <- avg_mh + 3*sd_mh
+  dat$mahal_dist <- dists
   dat$is_outlier <- (abs(dists) > ub)
   dat
 }
 
-new_dat <- properties %>% 
+new_dat <- temp_dist %>% 
+  mutate(region_zip = ifelse(is.na(region_zip), 0, region_zip)) %>% 
   split(.$region_zip) %>% 
   map(detect_outliers) %>% 
   do.call('rbind', .)
@@ -164,18 +106,173 @@ new_dat <- properties %>%
 
 
 
-(abs(dists) > ub) %>% sum
-prop97328[abs(dists) > ub,] %>% pull(logerror) %>% mean
+# reg95982 <- new_dat %>% inner_join(transactions, by = "id_parcel") %>% filter(region_zip == 95982)  
+#   
+# lat_and_lon <- cbind(reg95982$latitude,reg95982$longitude)
+# S <- cov(lat_and_lon)
+# mu <- colMeans(lat_and_lon)
+# dists <- mahalanobis(lat_and_lon, center = mu, cov = S)  
+# mean(dists)
+# max(dists)
+# min(dists)
+
+
+# has a shed or not
+# new_dat %>% mutate(has_shed = !is.na(area_shed)*1,
+#                    has_patio = !is.na(area_patio)*1,
+#                    has_fireplace = !is.na(flag_fireplace)*1,
+#                    has_pool = !is.na(num_pool)*1,
+#                    pool_type = as.factor(case_when(!is.na(pooltypeid10) ~ "spa or hot tub",
+#                                          !is.na(pooltypeid2) ~ "pool and tub",
+#                                          !is.na(pooltypeid7) ~ "pool only",
+#                                          TRUE ~ "nothing")),
+#                    tax_delinquency = 1*tax_delinquency)
+# 
+
+tmp[is.na(tmp)] <- 0
+
+na_to_zero <- function(x){
+  x[is.na(x)] <- 0
+  x
+}
+
+new_dat <- new_dat %>% mutate(needs_pred = 1)
+transactions <- transactions %>% mutate(is_train =1)
+
+complete_obs <- right_join(transactions, new_dat, by = "id_parcel")
+
+labels <- complete_obs$logerror
+datez <- complete_obs$date
+ids <- complete_obs$id_parcel
+complete_obs$row_number <- 1:nrow(complete_obs)
+has_label <- complete_obs %>% 
+  filter(!is.na(logerror)) %>% pull(row_number)
+
+complete_obs <- complete_obs %>% select(-date)
+complete_obs[is.na(complete_obs)] <- 0
+removed_feats <- complete_obs %>% 
+  mutate_at(vars(architectural_style, pooltypeid10,
+                 pooltypeid2,zoning_landuse_county,
+                 material, zoning_property, fips), as.factor) %>% 
+  select(-latitude, - longitude, -id_parcel,
+         -tax_delinquency_year, - rawcensustractandblock,
+         -censustractandblock, -zoning_property, -region_neighbor, -region_zip) 
 
 
 
-abs %>% `>`(3) %>% sum
+removed_feats %>% mutate_if(is.factor, as.numeric) %>% str
+ 
+train_inds <- sample(seq_len(nrow(removed_feats)), size = nrow(removed_feats) * 0.5)
 
-prop97328$latitude %>% length
+hashed_df <- hashed.model.matrix( logerror ~ . -1, data = as.data.frame(removed_feats), hash.size = 2^17)
+
+train_hashed <- hashed_df[has_label,]
+# val_hashed <- hashed_df[-train_inds,]
+train_label <- labels[has_label]
+# val_label <- labels[-train_inds]
+
+
+
+
+dtrain <- xgb.DMatrix(train_hashed, label = train_label)
+dval <- xgb.DMatrix(val_hashed, label = val_label)
+
+
+
+
+param <- list(objective = "reg:linear", 
+              eval_metric = "rmse",
+              eta = 0.1,
+              gamma = 1,
+              subsample = 0.7,
+              colsample_bytree = 0.7,
+              min_child_weight = 0,
+              max_depth = 14)
+
+
+set.seed(120)
+
+m2 <- xgb.train(data = dtrain,
+                param,
+                nrounds = 50,
+                watchlist = list(val = dval,model = dtrain),
+                nthread=4)
+
+
+
+xgb_grid_1 <- expand.grid(eta = c(0.1),
+                        max_depth = c(2,10),
+                        gamma= c(1,0.1),
+                        colsample_bytree = 0.7,
+                        subsample = 0.7,
+                        min_child_weight = c(0,5)
+)
+
+
+paramFrame <- split(xgb_grid_1, 1:nrow(xgb_grid_1))
+
+
+xgb.error <- function(err_func) {
+  return(function(preds, dtrain) {
+    labels <- xgboost::getinfo(dtrain, "label")
+    if (length(unique(labels))<3) {
+      preds <- thresh(preds)
+    }
+    err <- err_func(labels, preds)
+    return(list(metric = "error", value = err))
+  })
+}
+mae <- function(actual, preds) {
+  mean(abs(actual - preds))
+}
+
+
+print_params <- function(paramz) paste0(paste0(names(paramz),"=", paramz), collapse = ", ")
+models <- map(paramFrame, function(paramz) {
+  cat("Training using,", print_params(paramz), "\n")
+  return(xgb.train(data=dtrain,
+                   param=paramz,
+                   nthread = 4,
+                   nrounds = 150,
+                   objective = "reg:linear",
+                   eval_metric = xgb.error(mae),
+                   watchlist = list(eval = dval, 
+                                    model = dtrain)))
+})
+
+val_preds <- map(models, function(model) {
+  predict(model, dval)
+})
+
+
+map(val_preds, function(preds) { 
+  mae(val_label, preds)
+  })
+
+sample_sub <- read_csv("~/Documents/Kaggle/zillow/sample_submission.csv", n_max = 10)
+temp_dist %>% nrow
+
+preds <- data.frame(id_parcel = complete_obs$id_parcel[-has_label],
+               logerror = predict(models[[5]], xgb.DMatrix(hashed_df[-has_label,])))
+out_errors <- bind_rows(preds, transactions %>% select(id_parcel, logerror))
+
+unique_preds <- out_errors %>% group_by(id_parcel) %>%
+  dplyr::summarise(logerror = mean(logerror)) %>% 
+  transmute(ParcelId = id_parcel,
+            `201610` = logerror,
+            `201611` = logerror,
+            `201612` = logerror,
+            `201710` = logerror,
+            `201711` = logerror,
+            `201712` = logerror)
+write_csv(unique_preds, "~/Documents/Kaggle/zillow/preds/first_try.csv")
 
 
 
 
 
 
-  
+
+
+
+
